8315bf52ba64625433553cfe535c64e4cd9a2ad5
Henrique Torres <henrique.landin@grupoicts.com.br>
2023-05-18 16:03:30 -0300

    Initial commit

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { BovinoculturaComponent } from './bovinocultura/bovinocultura.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
 		HeaderComponent,
-		BovinoculturaComponent
+		BovinoculturaComponent,
+  FooterComponent
 	],
   imports: [
     CommonModule
   ],
 	exports: [
 		HeaderComponent,
-		BovinoculturaComponent
+		BovinoculturaComponent,
+		FooterComponent
 	]
 })
 export class ComponentsModule { }
