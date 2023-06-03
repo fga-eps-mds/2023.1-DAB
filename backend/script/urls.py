40e51a240f7731a51b874dd5a1d1624da6022663
Breno <brenob6@gmail.com>
2023-06-03 18:24:25 -0300

    add urls

---
+++ backend/script/urls.py
@@ -0,0 +1,9 @@
+HOST="https://servicodados.ibge.gov.br/api/v3/agregados/"
+
+URLS = {
+    "AVICULTURA":"915/periodos/-20/variaveis/1988|29|9588|9589?localidades=N3[all]&classificacao=12716[115236]",
+    "SUINOCULTURA":"1093/periodos/-20/variaveis/284|285?localidades=N3[all]&classificacao=12716[115236]",
+    "BOVINOCULTURA":"1092/periodos/-20/variaveis/284%7C285?localidades=N3[all]&classificacao=12716[115236]|18[992,55,56,111734,111735]",
+    "SAFRA":"1612/periodos/-20/variaveis/109|216|214|112?localidades=N3[all]&classificacao=81[2692,2702,2711,2713]"
+
+}
