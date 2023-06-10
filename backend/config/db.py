424eb6d0987617e76669f14072186b43aa9ded64
Breno <brenob6@gmail.com>
2023-06-09 23:30:57 -0300

    better lint rating

--- backend/config/db.py
+++ backend/config/db.py
@@ -1,7 +1,9 @@
+import os
+
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from pymongo.collection import Collection
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
@@ -10,4 +12,9 @@ DB_PASS = os.getenv('DB_PASS')
 
 uri = f"mongodb+srv://{DB_USER}:{DB_PASS}@cluster0.b5iy3zl.mongodb.net/?retryWrites=true&w=majority"
 
-db = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri, server_api=ServerApi('1'))
+db = client.DAB
+
+async def fetch_data(collection : Collection, search : dict):
+    response = collection.find_one(search)
+    return response
