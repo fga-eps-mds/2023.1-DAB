23e89727e9017a5afc1f556d95fa1933ff90879f
mayara-tech <mayara-alves-2011@hotmail.com>
2023-06-12 22:55:47 -0300

    adding all routes

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from routes.avicultura import avicultura
+from routes.suinocultura import suinocultura
+from routes.bovinocultura import bovinocultura
+from routes.safra import safra
 
 app = FastAPI()
 app.include_router(avicultura)
+app.include_router(suinocultura)
+app.include_router(bovinocultura)
+app.include_router(safra)
---
+++ backend/routes/bovinocultura.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, HTTPException
+from config.db import db, fetch_data
+from script.models import IbgeData
+
+bovinocultura = APIRouter()
+collection = db.bovinocultura
+
+@bovinocultura.get("/abatidos")
+async def bovinos_abatidos() -> IbgeData:
+    search = {"id" : "284"}
+    response = await fetch_data(collection, search)
+    if response is None:
+        raise HTTPException(status_code=404)
+    ibge_data : IbgeData = IbgeData(**response)
+    return ibge_data
+
+@bovinocultura.get("/peso")
+async def peso_carcaca_bovinos() -> IbgeData:
+    search = {"id" : "285"}
+    response = collection.find_one(search)
+    if response is None:
+        raise HTTPException(status_code=404)
+    ibge_data : IbgeData = IbgeData(**response)
+    return ibge_data
