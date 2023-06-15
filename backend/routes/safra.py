5ae2af56e9c6b1e021fcc568419f07d8bfe5c339
Breno <brenob6@gmail.com>
2023-06-15 16:50:59 -0300

    add prefix

--- backend/routes/safra.py
+++ backend/routes/safra.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 from config.db import db, fetch_data
 from script.models import IbgeData
 
-safra = APIRouter()
+safra = APIRouter(prefix="/safra")
 collection = db.safra
 
 @safra.get("/plantada")
