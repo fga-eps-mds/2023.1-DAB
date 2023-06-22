bf0bc907d036f7b81f8e4e40a126857426975c07
Breno <brenob6@gmail.com>
2023-06-22 20:21:03 -0300

    refactor routes

--- backend/routes/avicultura.py
+++ backend/routes/avicultura.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from fastapi import APIRouter, HTTPException
 from config.db import db, fetch_data
 from script.models import IbgeData
@@ -5,20 +6,27 @@ from script.models import IbgeData
 avicultura = APIRouter(prefix="/avinocultura")
 collection = db.avicultura
 
-@avicultura.get("/producao")
-async def quatidade_ovos_produzidos() -> IbgeData:
-    search = {"id" : "1988"}
+class Code(str, Enum):
+    poedeiras = "1988"
+    ovos_produzidos = "29"
+    producao_consumo = "9588"
+    producao_incubacao = "9589"
+    populacao = "2209"
+
+@avicultura.get("/{code}", tags=["avicultura"])
+async def dados_avicultura(code : Code) -> IbgeData:
+    """
+    **Code**
+    -   1988    : Número de cabeças de galinhas poeiras
+    -   29      : Quantidade de ovos produzidos
+    -   9588    : Quantidade de ovos produzidos para consumo
+    -   9589    : Quantidade de ovos produzidos para incubação
+    -   2209    : Número de cabeças
+    """
+    search = {"id" : code.value}
     response = await fetch_data(collection, search)
     if response is None:
         raise HTTPException(status_code=404)
     ibge_data : IbgeData = IbgeData(**response)
     return ibge_data
 
-@avicultura.get("/cabecas")
-async def numero_cabecas() -> IbgeData:
-    search = {"id" : "29"}
-    response = collection.find_one(search)
-    if response is None:
-        raise HTTPException(status_code=404)
-    ibge_data : IbgeData = IbgeData(**response)
-    return ibge_data
