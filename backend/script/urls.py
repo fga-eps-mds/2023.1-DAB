e95ce23b3df92d98f24acbfd71a0206c83ba4175
Breno <brenob6@gmail.com>
2023-06-30 13:49:27 -0300

    fix: URLs

--- backend/script/urls.py
+++ backend/script/urls.py
@@ -1,17 +1,9 @@
 HOST="https://servicodados.ibge.gov.br/api/v3/agregados/"
 
 URLS = {
+    "avicultura":"915/periodos/-20/variaveis/1988|29|9588|9589?localidades=N1[all]&classificacao=12716[115236]",
+    "suinocultura":"1093/periodos/-20/variaveis/284|285?localidades=N1[all]&classificacao=12716[115236]",
+    "bovinocultura":"1092/periodos/-20/variaveis/284%7C285?localidades=N1[all]&classificacao=12716[115236]|18[992,55,56,111734,111735]",
+    "safra":"1612/periodos/-20/variaveis/109|216|214|112?localidades=N3[all]&classificacao=81[2692,2702,2711,2713]"
 
-    "bovinocultura":"960/periodos/-20/variaveis/2209?localidades=N3[all]&classificacao=12443[110056]",
-    "suinocultura":"960/periodos/-20/variaveis/2209?localidades=N3[all]&classificacao=12443[110063]",
-    "avicultura":"960/periodos/-20/variaveis/2209?localidades=N3[all]&classificacao=12443[111191]",
-
-#"avicultura":"915/periodos/-20/variaveis/1988|29|9588|9589?localidades=N3[all]&classificacao=12716[115236]",
-#
-#    "suinocultura":"1093/periodos/-20/variaveis/284|285?localidades=N3[all]&classificacao=12716[115236]",
-#
-#    "bovinocultura":"1092/periodos/-20/variaveis/284%7C285?localidades=N3[all]&classificacao=12716[115236]|18[992,55,56,111734,111735]",
-#
-#    "safra":"1612/periodos/-20/variaveis/109|216|214|112?localidades=N3[all]&classificacao=81[2692,2702,2711,2713]"
-#
 }
