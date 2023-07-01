715253eb653bd2f4f1e10d839cc87b938e64195c
Breno <brenob6@gmail.com>
2023-07-01 15:35:31 -0300

    add ChartData

--- backend/script/models.py
+++ backend/script/models.py
@@ -8,11 +8,24 @@ class Series(BaseModel):
     localidade: Localidade
     serie: dict[str,str]
 
+class Classificacoes(BaseModel):
+    categoria: dict[str, str]
+
 class Resultados(BaseModel):
     series: list[Series]
+    classificacoes: list[Classificacoes]
 
 class IbgeData(BaseModel):
     id: str
     variavel: str
     unidade: str
     resultados: list[Resultados]
+
+class ChartData(BaseModel):
+    id: str
+    locale: str
+    categoria: None | str
+    type: str
+    title: str
+    labels: list[str]
+    dataList: list[str]
