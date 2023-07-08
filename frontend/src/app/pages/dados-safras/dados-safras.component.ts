25e62d6efe5688eabce15cdab0170fedadabf2db
Muniz2811 <pedrohenben2014@gmail.com>
2023-07-07 23:07:17 -0300

    Criando dados safras

--- frontend/src/app/pages/dados-safras/dados-safras.component.ts
+++ frontend/src/app/pages/dados-safras/dados-safras.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SafraService } from 'src/app/services/safra-service/safra.service';
+import { ChartData } from 'src/app/interfaces/ChartData';
+import { Card } from 'src/app/interfaces/Card';
 
 @Component({
   selector: 'app-dados-safras',
@@ -6,5 +10,93 @@ import { Component } from '@angular/core';
   styleUrls: ['./dados-safras.component.scss']
 })
 export class DadosSafrasComponent {
+
+  cards: Card[] = [{
+    id: 'arroz',
+    imagemUrl: './assets/porco.png',
+    nome: 'Arroz',
+    desc1: 'Dados referentes à',
+    desc2: 'suinocultura do último',
+    desc3 : 'trimestre'
+  }, 
+  {
+    id: 'feijao',
+    imagemUrl: './assets/vaquinha.png',
+    nome: 'Feijao',
+    desc1: 'Dados referentes à',
+    desc2: 'bovinocultura do último',
+    desc3 : 'trimestre'
+  }, 
+  {
+    id: 'milho',
+    imagemUrl: './assets/galinha.png',
+    nome: 'Milho',
+    desc1: 'Dados referentes à',
+    desc2: 'avicultura do último',
+    desc3 : 'trimestre'
+  },
+  {
+    id: 'soja',
+    imagemUrl: './assets/galinha.png',
+    nome: 'Soja',
+    desc1: 'Dados referentes à',
+    desc2: 'avicultura do último',
+    desc3 : 'trimestre'
+  }
+
+
+]
+
+  charts: Array<ChartData> = [];
+	load: boolean = false;
+
+  constructor(
+    private route:ActivatedRoute,
+    private safraService:SafraService,
+
+  )
+  { }
+
+  getSafra(type:string){
+    const routerParams = this.route.snapshot.paramMap;
+		const EstadoID = routerParams.get("EstadoID") ?? "";
+    this.safraService.request(type, EstadoID).subscribe((res)=>{
+      this.charts=res;
+      this.load = true;
+      
+    });
+  }
+
+  ngOnInit() {
+		//this.charts = this.loadData();
+		
+	}
+
+  loadData(): Array<ChartData>{
+    const routerParams = this.route.snapshot.paramMap;
+		const EstadoID = routerParams.get("EstadoID") ?? "";
+    
+    let charts: Array<ChartData> = [];
+
+    this.safraService.listArroz(EstadoID).subscribe((res) =>{
+        charts.push(res);
+    })
+
+    this.safraService.listFeijao(EstadoID).subscribe((res) =>{
+      charts.push(res);
+    })
+
+    this.safraService.listMilho(EstadoID).subscribe((res) =>{
+      charts.push(res);
+    })
+
+    this.safraService.listSoja(EstadoID).subscribe((res) =>{
+      charts.push(res);
+    })
+    
+    this.load = true;
+    return charts;
+  }
+
 
 }
