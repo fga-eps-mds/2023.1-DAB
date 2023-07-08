750c4f99eaea0388d75826e18a1843418049cba3
Muniz2811 <pedrohenben2014@gmail.com>
2023-07-07 23:00:28 -0300

    Modificando endpoint safras

--- backend/routes/safra.py
+++ backend/routes/safra.py
@@ -7,21 +7,15 @@ safra = APIRouter(prefix="/safra")
 COLLECTION = "SAFRA"
 
 class Code(str, Enum):
-    AREA_PLANTADA = "109"
-    AREA_COLHIDA = "216"
-    PRODUCAO = "214"
-    RENDIMENTO = "112"
+    ARROZ = "arroz"
+    MILHO = "milho"
+    FEIJAO = "feijao"
+    SOJA = "soja"
 
 @safra.get("/{code}/{locale}")
 async def dados_safra(code : Code, locale: str):
-    """
-    **Code:**
-    -   109 : Area plantada
-    -   216 : Area colhida
-    -   214 : Produção
-    -   112 : Rendimento
-    """
-    search = {"id": code.value, "locale" : locale}
+   
+    search = {"categoria": code.value, "locale" : locale}
     response = await fetch_list(COLLECTION, search)
     if response is None:
         raise HTTPException(status_code=404)
